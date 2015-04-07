adb301fcbd06a59119419ddfd6e85882fedb67a0
Chris Marx <chrismarx@gmail.com>
2015-04-07 17:48:00 -0400

    added jquery for protractor testing, added more protractor scenarios

--- examples/angularjs-perf/test/e2e/scenarios.js
+++ examples/angularjs-perf/test/e2e/scenarios.js
@@ -3,8 +3,9 @@ var TodoPage = function() {
   this.newTodoInput = element(by.model('newTodo'));
 
   this.footer = element(by.id('footer'));
-  this.remainingCount = element(by.binding('remainingCount'));
+  this.remainingCount = element(by.exactBinding('remainingCount'));
 	this.clearCompletedButton = element(by.id('clear-completed'));
+  this.clearCompletedCount = element(by.exactBinding('todos.length - remainingCount'));
 	this.todoList = element.all(by.repeater('todo in todos'));
 
   this.get = function() {
@@ -16,7 +17,16 @@ var TodoPage = function() {
 };
 
 describe('todomvc', function() {
+
+  //start the tests with the main page
   browser.get('/');
+
+  //at beginning of test, clear local storage, so we're starting clean
+  browser.executeScript(function() {
+    var todoStorage = angular.element(document.body).injector().get('todoStorage');
+    todoStorage.clear();
+  });
+
 
   it("should load", function() {
   	expect(browser.getTitle()).toBe('AngularJS • TodoMVC');
@@ -26,6 +36,7 @@ describe('todomvc', function() {
 
   describe('open page', function() {
 	  it('should have a hidden footer', function() {
+      todoPage.clearCompletedButton.getText().then(console.log);
 	  	expect(todoPage.footer.isDisplayed()).toBeFalsy();
 	  });
 
@@ -55,7 +66,12 @@ describe('todomvc', function() {
   	});
 
   	it('should show correct remaining count', function() {
-  		expect(todoPage.remainingCount).toEqual(1);
+      todoPage.remainingCount.getText().then(function(txt){
+        expect(txt == 1).toBeTruthy();
+      });
+
+      //this method failed, perhaps because of the duplicate matches on the page?
+      //expect(todoPage.remainingCount).toEqual(1);
   	});
 
   	it('should not display clear completed button', function() {
@@ -64,48 +80,58 @@ describe('todomvc', function() {
   });
 
 
-  xdescribe('completing a todo', function() {
+  describe('completing a todo', function() {
 
   	it('should mark a todo as completed', function() {
 	  	var todo = element.all(by.repeater('todo in todos')).get(0);
-      todo.element(by.binding('remainingCount')).click();
+      todo.element(by.model('todo.completed')).click();
 
-      browser.executeScript('return window;').then(function(window){
-        expect(window.getComputedStyle(angular.element($0)[0], null)["text-decoration"]).toEqual('1.3.0-rc.3');  
+      //sure would be nice to return window, or pass in todo, and do the lookup using todo[0] rather than finding it on the page...
+      browser.executeScript(function(){
+        return window.getComputedStyle($("#todo-list li:eq(0)").find("label")[0],null)["text-decoration"];
+      }).then(function(textDecoration){
+        expect(textDecoration).toEqual('line-through');  
       });
      
   	});
 
-  	it('should...', function() {
-			element.all(by.repeater('todo in todos')).get(0).click();
+    it('should show correct remaining count', function() {
+      todoPage.remainingCount.getText().then(function(txt){
+        expect(txt == 0).toBeTruthy();
+      });
+      
+      //this method failed, perhaps because of the duplicate matches on the page?
+      //expect(todoPage.remainingCount).toEqual(1);
+    });
 
+    it('should display clear completed button and have correct count', function() {
+      expect(todoPage.clearCompletedButton.isDisplayed()).toBeTruthy();
+      todoPage.clearCompletedCount.getText().then(function(txt){
+        //replace any text and make sure the count matches
+        expect(parseInt(txt.replace(/\D+/,''))).toEqual(1);
+      })
+    });
+
+  	it('should display correct number of completed tasks on the completed task view', function() {
+			element(by.css("a[href='#/completed']")).click();
+
+      expect(browser.getCurrentUrl()).toMatch(/\/completed/);
+
+      expect(todoPage.todoList.count()).toEqual(1);
   	});
 
   });
 
   
 
+  
+
 });
 
 
-// *open page
-// status bar should not be visible 
-// list should be empty
-// focus should be on input
-
-// *enter todo
-// list should add 1
-// remaining should be 1
-// status bar should be visible
-// focus should be on input
-// should add to bottom of list
-
+//TODO
 // *hover over todo and click X
 // todo should be deleted
-
-// *click checkmark
-// "clear completed" button should be visible
-// remaining count should drop
 
 // *double click todo
 // todo should become editable
@@ -113,7 +139,5 @@ describe('todomvc', function() {
 // todo should change and become uneditable
 
 // *switch to active tab
-
-// * switch to completed tab
 
 // toggle all
